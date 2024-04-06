90299a7465dcca8450d65d98415c6a9dc500e950
RajanKannaujiya <rajankannaujiya2034@gmail.com>
2024-04-06 15:29:37 +0530

    fixed bugs

--- backend/Components/index.js
+++ backend/Components/index.js
@@ -42,7 +42,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/IndividualChat', {
+  mongoose.connect('mongodb://127.0.0.1:27017/IndividualChat', {
     family: 4,
   });
 }
